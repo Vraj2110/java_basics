c7f2a875dbd009cad624767af0b2624be53e1089
Vraj Desai <desaivraj18@gmail.com>
2026-05-12 21:54:00 +0530

    complete a arithmetic and increment and decremnet operator

---
+++ com.javabasics/datatypes/src/arithmetic.java
@@ -0,0 +1,21 @@
+public class arithmetic {
+
+    static void main(String[] args) {
+
+//arithmetic operation
+    int a = 5 + 3;
+        int b = 10 - 4;
+//        int c = 5 * 5;
+//        int d = 10 / 3; //float e = 10.0 / 3.0;
+//        double e = 10.0 / 3.0d;
+//        int f = 10 % 3;
+
+//        System.out.println(e);
+        a = 5; //shortcut
+         a += 5;
+         b -= 3;
+
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
---
+++ com.javabasics/datatypes/src/increment_operator.java
@@ -0,0 +1,18 @@
+public class increment_operator {
+
+    static void main(String[] args) {
+//        int a = 1;
+//        System.out.println(a);
+//        a++;
+//        System.out.println(a); // increment operator
+//        ++a;
+//        System.out.println(a);
+//        a--;
+//        System.out.println(a);
+//        --a;
+//        System.out.println(a);
+
+        int z = 3;
+        System.out.println(z++ + --z); //postincrement means it is incremenet afte a  next expression means here it is stay 3 when z++ and then it change 4-1=3 for --z and them it plus for 3 + 3 = 6
+    }
+}
