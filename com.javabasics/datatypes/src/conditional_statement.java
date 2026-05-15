17677e1ff8d95798768c31315b650f9793bc78c1
Vraj Desai <desaivraj18@gmail.com>
2026-05-15 23:34:40 +0530

    if and loop complete next is array

---
+++ com.javabasics/datatypes/src/conditional_statement.java
@@ -0,0 +1,47 @@
+public class conditional_statement {
+    static void main(String[] args) {
+//        boolean israin = false;
+//        boolean issuuny = false;
+
+//        if (israin) { //if,if else and if else ladder
+//            System.out.println("bring an umbrella");
+//        }else if (issuuny){
+//            System.out.println("Wear an sun glasses");
+//        }else{
+//            System.out.println("Just go outside");
+//        }
+
+//        int roll = 21;  switch case
+//        switch (roll){
+//            case 1:
+//                System.out.println("dev");
+//                break;
+//            case 2:
+//                System.out.println("dhruv");
+//                break;
+//            case 3:
+//                System.out.println("het");
+//                break;
+//            case 4:
+//                System.out.println("neel");
+//                break;
+//            case 5:
+//                System.out.println("fenil");
+//                break;
+//            case 21:
+//                System.out.println("Vraj Desai");
+//                break;
+//            case 22:
+//                System.out.println("heer");
+//                break;
+//            case 7:
+//                System.out.println("yash");
+//                break;
+//            default:
+//                System.out.println("no roll number found");
+//        }
+
+        int time = 20; //ternarry operator
+        System.out.println((time>18) ? "good moring." : "good night.");
+    }                                     //true           //false
+}
---
+++ com.javabasics/datatypes/src/loop.java
@@ -0,0 +1,18 @@
+public class loop {
+    static void main(String[] args) {
+//        int i = 1;
+//        while (i<101){
+//            System.out.println(i); //while loop
+//            i++;
+//        }
+//        for ( int p = 1; p<=10; p++){
+//            System.out.println("2 *" + p + "="+ (2 * p));//for loop
+//        }
+
+        int v = 10;
+        do {
+            System.out.println("v  is " + v);// do while it run one time if condition is false
+            v++;
+        }while (v < 5);
+    }
+}
