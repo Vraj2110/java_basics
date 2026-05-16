3b298a2fd9bee647b74c647cc0892073c774bb53
Vraj Desai <desaivraj18@gmail.com>
2026-05-16 23:38:23 +0530

    array complete next oops

---
+++ com.javabasics/datatypes/src/array.java
@@ -0,0 +1,21 @@
+public class array {
+    static void main(String[] args) {
+//            int[] a = new int[5];
+//            a[0] = 10;
+//            a[1] = 20;
+//            a[2] = 30;
+//            a[3] = 40;
+//            a[4] = 50;
+//
+//        System.out.println(a);
+//        for (int i = 0;i<5;i++){
+//            System.out.println(a[i]);
+//        }
+//        for (int c : a){//for each loop
+//            System.out.println(c);
+//        }
+
+        String [] cars = {"bmw", "volvo","Ford"};
+        System.out.println(cars[2]);
+    }
+}
